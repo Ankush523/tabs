bf42bd966cf70249d91762cef61c24090c4f5e6a
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-12 08:47:20 +0530

    fetching jobs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+const url = 'https://course-api.netlify.app/api/react-tabs-projects';
 function App() {
+
+  const[loading,setLoading] = useState(true)
+  const[jobs,setJobs] = useState([])
+  const[value,setValue] = useState(0)
+
+  const fetchJobs = async() => {
+    const response = await fetch(url)
+    const newJob = await response.json()
+    setJobs(newJob)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
